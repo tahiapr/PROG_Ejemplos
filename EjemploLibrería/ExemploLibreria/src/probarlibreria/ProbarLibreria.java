d5876ed0c97be4d3235f2ff4ef210a58989cc255
tperezrodriguez <tperezrodriguez@victoria5.danielcastelao.org>
2018-01-24 11:39:57 +0100

    Ejemplo Librería

---
+++ EjemploLibrería/ExemploLibreria/src/probarlibreria/ProbarLibreria.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package probarlibreria;
+
+import rectangulo.Rectangulo;
+import triangulo.Triangulo;
+
+/**
+ *
+ * @author tperezrodriguez
+ */
+public class ProbarLibreria {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Triangulo.areaTriangulo(5f, 12f);
+        Rectangulo.areaRectangulo(3f, 5f);
+    }
+    
+}
